9ed1f850c8e8636ee9d5fe55bbc3dc0e3ace62fb
jiseonBaek <js.coding.place@gmail.com>
2022-03-06 01:14:49 +0900

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({ //that function called decorator can add functionaliry to a Class
-  imports: [UsersModule, EmailModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: (process.env.NODE_ENV === 'production') ? '.production.env' : (process.env.NODE_ENV === 'development') ? '.development.env' : '.stage.env'
+  })],
   controllers: [], //takes url and excute functions like router of nodejs
   providers: [],
 })
