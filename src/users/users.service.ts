829330f5c5676ed6a15edf410568f0fe9f84f352
jiseonBaek <js.coding.place@gmail.com>
2022-03-05 22:47:35 +0900

    add user service for login&verify email&getUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import * as uuid from 'uuid';
 import { Injectable } from '@nestjs/common';
 import { EmailService } from 'src/email/email.service';
+import { UserInfo } from './UserInfo';
 
 @Injectable()
 export class UsersService {
@@ -29,6 +30,21 @@ export class UsersService {
     }
 
     async verifyEmail(signupVerifyToken: string): Promise<string> {
+        //1. DB에서 signupVerifyToken으로 회원가입 처리중인 유저가 있는지 조회하고 없다면 에러처리
+        //2. 바로 로그인 상태가 되도록 JWT를 발급
+        
+        throw new Error('Method not implemented');
+    }
+
+    async login(email: string, password: string): Promise<string> {
+        //1.email,password를 가진 유저가 존재하는지 DB확인, 없다면 에러 처리
+        //2. JWT 발급
+        throw new Error('Method not implemented');
+    }
+
+    async getUserInfo(userId: string): Promise<UserInfo> {
+        //1. userId를 가진 유저가 존재하는지 DB 확인, 없으면 에러 처리
+        //2. 조회된 데이터를 UserInfo 타입으로 응답
         throw new Error('Method not implemented');
     }
 }
