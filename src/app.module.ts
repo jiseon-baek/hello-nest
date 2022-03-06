a09baa98071ba15ec15f5dbf797a24267ae314a2
jiseonBaek <js.coding.place@gmail.com>
2022-03-06 22:25:24 +0900

    separate config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
 @Module({ //that function called decorator can add functionaliry to a Class
-  imports: [ConfigModule.forRoot({
-    envFilePath: (process.env.NODE_ENV === 'production') ? '.production.env' : (process.env.NODE_ENV === 'development') ? '.development.env' : '.stage.env'
-  })],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    })
+  ],
   controllers: [], //takes url and excute functions like router of nodejs
   providers: [],
 })
